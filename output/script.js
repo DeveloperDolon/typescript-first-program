3d9bc2879161aa81a02eabea8e103483ee0671ad
Dolon Roy <dolonr718@gmail.com>
2023-12-06 21:09:26 +0600

    project started commit

---
+++ output/script.js
@@ -0,0 +1,12 @@
+"use strict";
+const main = (param) => {
+    console.log("Hello world!", param);
+};
+const person = {
+    name: "Dolon Chandra",
+    age: 35,
+    isDeveloper: true
+};
+const roll = 45;
+console.log(person, roll);
+main("Dolon Chandra Roy");
---
+++ src/script.ts
@@ -0,0 +1,17 @@
+
+
+const main = (param: string) => {
+    console.log("Hello world!", param);
+}
+
+const person = {
+    name: "Dolon Chandra",
+    age: 35,
+    isDeveloper: true
+}
+
+const roll: number = 45;
+
+console.log(person, roll)
+
+main("Dolon Chandra Roy");
